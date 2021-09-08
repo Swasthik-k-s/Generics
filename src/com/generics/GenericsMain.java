f265a8bea5375f68631095f24604d4dd4d8c359d
Swasthik <swasthik.ks22@gmail.com>
2021-09-08 19:27:08 +0530

    Extended max method to take more than 3 parameters

--- src/com/generics/GenericsMain.java
+++ src/com/generics/GenericsMain.java
@@ -3,21 +3,21 @@ package com.generics;
 public class GenericsMain {
 
 	//Instance Variables
-	static Integer a1 = 20, b1 = 52, c1 = 49;
-	static Float a2 = 93.f, b2 = 43.9f, c2 = 23f;
+	static Integer a1 = 20, b1 = 52, c1 = 49, d1 = 72;
+	static Float a2 = 93.f, b2 = 43.9f, c2 = 23f, d2 = 82f, e2 = 12f;
 	static String a3 = "Generic", b3 = "Maximum", c3 = "Problem";
 	
 	public static void main(String[] args) {
 		
 		//Creating 3 objects to find the maximum for Integer, Float and String
-		MaxNumber<Integer> max1 = new MaxNumber<>(a1,b1,c1);
-		MaxNumber<Float> max2 = new MaxNumber<>(a2,b2,c2);
-		MaxNumber<String> max3 = new MaxNumber<>(a3,b3,c3);
+		MaxNumber<Integer> max1 = new MaxNumber<>();
+		MaxNumber<Float> max2 = new MaxNumber<>();
+		MaxNumber<String> max3 = new MaxNumber<>();
 		
 		//Calling findMax method the find the maximum value
-		max1.findMax();
-		max2.findMax();
-		max3.findMax();
+		max1.testMaximum(a1,b1,c1,d1);
+		max2.testMaximum(a2,b2,c2,d2,e2);
+		max3.testMaximum(a3,b3,c3);
 	}
 
 }
--- src/com/generics/MaxNumber.java
+++ src/com/generics/MaxNumber.java
@@ -1,29 +1,17 @@
 package com.generics;
 
+import java.util.*;
+
 public class MaxNumber<T extends Comparable<T>> {
 
-	private T a,b,c;
-	
-	public MaxNumber(T a, T b, T c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
-
-	//Method used to call generic method from main
-	public void findMax() {
-		testMaximum(a,b,c);
-	}
-	
-	//Method to find the maximum value taking 3 parameters
-	public <T extends Comparable<T>> void testMaximum(T a,T b,T c) {
-		T max = a;
-		if(b.compareTo(max)>0) {
-			max = b;
+	//Method to find the maximum value taking n parameters
+	public <T extends Comparable<T>> void testMaximum(T... value) {
+		List<T> array = new ArrayList<T>();
+		
+		for(T item: value) {
+			array.add(item);
 		}
-		if(c.compareTo(max)>0) {
-			max = c;
-		}
-		System.out.println("Maximum of (" + a + "," + b + "," + c + ") = " + max);
+		array.sort(null);
+		System.out.println("Maximum = " + array.get(array.size()-1));
 	}
 }
