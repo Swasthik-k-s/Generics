93dbf978a85654cd8620b952df08f29255ac2736
Swasthik <swasthik.ks22@gmail.com>
2021-09-08 19:02:31 +0530

    Created Generic class to find the maximum

--- src/com/generics/GenericsMain.java
+++ src/com/generics/GenericsMain.java
@@ -2,17 +2,22 @@ package com.generics;
 
 public class GenericsMain {
 
+	//Instance Variables
+	static Integer a1 = 20, b1 = 52, c1 = 49;
+	static Float a2 = 93.f, b2 = 43.9f, c2 = 23f;
+	static String a3 = "Generic", b3 = "Maximum", c3 = "Problem";
+	
 	public static void main(String[] args) {
 		
 		//Creating 3 objects to find the maximum for Integer, Float and String
-		MaxNumber max1 = new MaxNumber();
-		MaxNumber max2 = new MaxNumber();
-		MaxNumber max3 = new MaxNumber();
+		MaxNumber<Integer> max1 = new MaxNumber<>(a1,b1,c1);
+		MaxNumber<Float> max2 = new MaxNumber<>(a2,b2,c2);
+		MaxNumber<String> max3 = new MaxNumber<>(a3,b3,c3);
 		
 		//Calling findMax method the find the maximum value
-		max1.findMax(20,523,82);
-		max2.findMax(93f,43.9f,23f);
-		max3.findMax("genric","maximum","problem");
+		max1.findMax();
+		max2.findMax();
+		max3.findMax();
 	}
 
 }
--- src/com/generics/MaxNumber.java
+++ src/com/generics/MaxNumber.java
@@ -1,9 +1,22 @@
 package com.generics;
 
-public class MaxNumber {
+public class MaxNumber<T extends Comparable<T>> {
 
+	private T a,b,c;
+	
+	public MaxNumber(T a, T b, T c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	//Method used to call generic method from main
+	public void findMax() {
+		testMaximum(a,b,c);
+	}
+	
 	//Method to find the maximum value taking 3 parameters
-	public <T extends Comparable<T>> void findMax(T a,T b,T c) {
+	public <T extends Comparable<T>> void testMaximum(T a,T b,T c) {
 		T max = a;
 		if(b.compareTo(max)>0) {
 			max = b;
